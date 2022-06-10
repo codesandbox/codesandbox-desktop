4059e78cb385e17c87f72a02a6d5226efd23ef76
Danilo Woznica <danilowoz@gmail.com>
2022-06-10 23:00:20 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, screen } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width, height } = primaryDisplay.workAreaSize;
+
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: width,
+    height: height,
+    titleBarStyle: "hidden",
+    frame: false,
+    titleBarStyle: "hiddenInset",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      webviewTag: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile("index.html");
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
