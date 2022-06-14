7c752005e3418f4f51cd04a4b27473c4b1527d95
Ives van Hoorne <ives@codesandbox.io>
2022-06-14 22:32:57 +0200

    add packaging

--- main.js
+++ main.js
@@ -24,8 +24,10 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile("index.html");
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (process.env.NODE_ENV === "development") {
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  }
 };
 
 // This method will be called when Electron has finished
